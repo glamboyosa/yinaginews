a798df950b55d81abc20cfa1ada0f37a35912206
glamboyosa <ogbemudiatimothy@gmail.com>
2019-08-19 03:30:29 +0100

    removed unused imports in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import USNews from "./Components/USNewsComponent/usNewsComponent";
-import HomeContainer from "./Components/homeComponent/homeContainer";
 import Routes from "./Components/Routes/routes";
 class App extends Component {
   render() {
